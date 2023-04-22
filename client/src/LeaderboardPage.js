7d8b8057cc1b59aaea47ce63668ed83fa33ec9b1
Charles Bucquet <charles@bucquet.com>
2023-04-22 10:53:36 -0700

    Started work on home page

--- client/src/LeaderboardPage.js
+++ client/src/LeaderboardPage.js
@@ -1,11 +1,12 @@
 import React from "react";
+import { Heading } from '@chakra-ui/react'
 import "./LeaderboardPage.css";
 
 function LeaderboardPage() {
 
     return (
     <div id="leaderboard">
-        <h1>Leaderboard Page</h1>
+        <Heading>Leaderboard Page</Heading>
         
     </div>
     );
