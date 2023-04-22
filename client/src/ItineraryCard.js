82d0410c49b51e905ca37b8840c00893a41917ad
marcuscheng123 <marcusdcheng@gmail.com>
2023-04-22 14:58:51 -0700

    Leaderboard 3

---
+++ client/src/ItineraryCard.js
@@ -0,0 +1,110 @@
+import React from "react";
+import { useState } from 'react';
+import { Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Text, Divider, ButtonGroup, Button, Flex, Avatar, Box, IconButton, Link, UnorderedList, ListItem, ListIcon, VStack, HStack } from '@chakra-ui/react';
+import { Icon, CheckIcon, } from '@chakra-ui/icons';
+import { MdAttractions, MdOutlineFastfood, MdOutlineThumbUpOffAlt } from 'react-icons/md';
+import "./ProfilePage.css";
+
+function ItineraryCard(props) {
+
+    const [itinerary, setItinerary] = useState(false);
+    const [liked, setLiked] = useState(false);
+    
+    return (
+    <div id="itineraryCard">
+        <Card maxW='lg'>
+            <CardHeader>
+                <Flex spacing='4'>
+                <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
+                    <Box>
+                    <Heading size='md'>{props.name}</Heading>
+                    <Text>{props.location}</Text>
+                    </Box>
+                </Flex>
+                <Button flex='1' variant='ghost' backgroundColor={liked ? 'brand.200' : 'white' } _hover='brand.200' onClick={() => {setLiked(!liked)}} leftIcon={<Icon as={MdOutlineThumbUpOffAlt} />}>
+                Like ({props.likes})
+                </Button>
+                </Flex>
+            </CardHeader>
+            <Image
+                objectFit='cover'
+                src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                alt='Chakra UI'
+                margin="5"
+            />
+            <CardBody>
+                <HStack spacing="auto">
+                <Text>
+                Total Price: ${props.total_price}
+                </Text>
+                <Text>
+                Budget: ${props.budget}
+                </Text>
+                </HStack>
+            </CardBody>
+            <CardFooter
+                justify='space-between'
+                flexWrap='wrap'
+                sx={{
+                '& > button': {
+                    minW: '136px',
+                },
+                }}
+            >
+            <VStack align="stretch" width="100%">
+            <Button color='teal.500' href='#' onClick={() => {setItinerary(!itinerary)}}>
+                {itinerary ? "Close Itinerary" : "View Itinerary"}
+            </Button>
+            {itinerary === true && <VStack align="stretch">
+                <Heading size='lg' align="center">
+                    Itinerary
+                </Heading>
+                <Divider />
+                <HStack spacing="auto" width="100%">
+                <Heading size='sm' marginBottom="3" marginTop="3">
+                    Hotel: {props.hotel}
+                </Heading>
+                <Text>Money Spent: ${props.hotel_price}</Text>
+                </HStack>
+                <Divider />
+                {props.days.map((day,num) => (
+                <>
+                <Heading size='sm' marginTop="2" marginBottom="2">
+                    Day {num+1}:
+                </Heading>
+                <VStack alignItems="start">
+                    <HStack>
+                        <Icon as={MdAttractions} color='green.500' />
+                        <Text>Activity: {day.attraction}</Text>
+                    </HStack>
+                    <HStack>
+                        <Icon as={MdOutlineFastfood} color='green.500' />
+                        <Text>Lunch: {day.food1}</Text>
+                    </HStack>
+
+                    <HStack>
+                        <Icon as={MdOutlineFastfood} color='green.500' />
+                        <Text>Dinner: {day.food2}</Text>
+                    </HStack>
+                </VStack>
+                <HStack spacing="auto" width="100%">
+                <Text marginTop="2" marginBottom="2">
+                    Distance: {day.miles_traveled}
+                </Text>
+                <Text marginTop="2" marginBottom="2">
+                    Money Spent: ${day.daily_price}
+                </Text>
+                </HStack>
+                </>
+                ))}
+                
+                <Divider />
+            </VStack>}
+          </VStack>
+        </CardFooter>
+      </Card>
+    </div>
+    );
+}
+
+export default ItineraryCard;
--- client/src/LeaderboardPage.js
+++ client/src/LeaderboardPage.js
@@ -1,52 +1,58 @@
 import React from "react";
 import { useState } from 'react';
+import ItineraryCard from "./ItineraryCard";
+import { MdAttractions, MdOutlineFastfood } from 'react-icons/md';
 import "./LeaderboardPage.css";
 import { Card, CardHeader, CardBody, CardFooter, Image, Stack, Heading, Text, Divider, ButtonGroup, Button, Flex, Avatar, Box, IconButton, Link, UnorderedList, ListItem, ListIcon } from '@chakra-ui/react';
 import { VStack } from "@chakra-ui/react";
-import { CheckIcon, CalendarIcon, HamburgerIcon } from '@chakra-ui/icons'
+import { Icon, CheckIcon, CalendarIcon, HamburgerIcon } from '@chakra-ui/icons'
 
 function LeaderboardPage() {
     const userList = [
         {
+            username: "Marcus Cheng",
             location: "Cleveland",
             hotel: "Motel 6",
             total_price: 7278,
             budget: 9341,
             num_likes: 69,
+            hotel_price: 384,
             days: [{
                 attraction: "Some Bridge",
                 food1: "The Worst Restaurant",
                 food2: "Canes",
-                daily_budget: 3421,
+                daily_price: 3421,
                 miles_traveled: 420,
             },
             {
                 attraction: "The Best Park in Cleveland",
                 food1: "Food Truck 21",
                 food2: "Taco Bell",
-                daily_budget: 3429,
+                daily_price: 3429,
                 miles_traveled: 81,
             },
             ]
         },
         {
+            username: "Joe Bruin",
             location: "Mexico City",
             hotel: "Motel 7",
             total_price: 3478,
             budget: 4823,
             num_likes: 421,
+            hotel_price: 456,
             days: [{
                 attraction: "Antropology Museum",
                 food1: "Tacos 1986",
                 food2: "Taco Bell",
-                daily_budget: 1243,
+                daily_price: 1243,
                 miles_traveled: 75,
             },
             {
                 attraction: "Aztec Pyramids",
                 food1: "Food Truck 22",
                 food2: "Del Taco",
-                daily_budget: 2110,
+                daily_price: 2110,
                 miles_traveled: 72,
             },
             ]
@@ -70,90 +76,7 @@ function LeaderboardPage() {
     <div id="leaderboard">
     <VStack spacing={10}>
     {userList.map((user,index) => (
-        <Card maxW='lg' key={index}>
-            <CardHeader>
-                <Flex spacing='4'>
-                <Flex flex='1' gap='4' alignItems='center' flexWrap='wrap'>
-                    <Box>
-                    <Heading size='md'>{user.location}</Heading>
-                    <Text>{user.location}</Text>
-                    </Box>
-                </Flex>
-                <Button flex='1' variant='ghost' leftIcon={<CheckIcon />}>
-                Like
-                </Button>
-                </Flex>
-            </CardHeader>
-            <Image
-                objectFit='cover'
-                src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
-                alt='Chakra UI'
-            />
-            <CardBody>
-                <Text>
-                Total Price: ${user.total_price}
-                </Text>
-                <Text>
-                Budget: ${user.budget}
-                </Text>
-            </CardBody>
-            <CardFooter
-                justify='space-between'
-                flexWrap='wrap'
-                sx={{
-                '& > button': {
-                    minW: '136px',
-                },
-                }}
-            >
-            <VStack>
-            {itinerary[index] === true && <Text>
-                <Heading size='lg' marginBottom="3">
-                    Itinerary
-                </Heading>
-                <Divider />
-                <Heading size='sm' marginBottom="3" marginTop="3">
-                    Hotel: {user.hotel}
-                </Heading>
-                <Divider />
-                {user.days.map((day,num) => (
-                <>
-                <Heading size='sm' marginTop="2" marginBottom="2">
-                    Day {num+1}:
-                </Heading>
-                <UnorderedList spacing={3}>
-                    <ListItem>
-                        <ListIcon as={CalendarIcon} color='green.500' />
-                        Activity: {day.attraction}
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon as={HamburgerIcon} color='green.500' />
-                        Lunch: {day.food1}
-                    </ListItem>
-                    <ListItem>
-                        <ListIcon as={HamburgerIcon} color='green.500' />
-                        Dinner: {day.food2}
-                    </ListItem>
-                </UnorderedList>
-                <Text marginTop="2" marginBottom="2">
-                    Money Spent: ${day.daily_budget}
-                </Text>
-                </>
-                ))}
-                
-                <Divider />
-            </Text>}
-            <Text>
-            {itinerary[index] === false && <Link color='teal.500' href='#' onClick={() => {changeItinerary(true, index)}}>
-                View Itinerary
-            </Link>}
-            {itinerary[index] === true && <Link color='teal.500' href='#' onClick={() => {changeItinerary(false, index)}}>
-                Close Itinerary
-            </Link>}
-            </Text>
-            </VStack>
-            </CardFooter>
-            </Card>
+        <ItineraryCard name={user.username} location={user.location} budget={user.budget} total_price={user.total_price} likes={user.num_likes} days={user.days} hotel={user.hotel} hotel_price={user.hotel_price} />
       ))}
     </VStack>
       
