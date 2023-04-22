1376dada7907c25744ca1a3627e84a0c4e7bfa68
Charles Bucquet <charles@bucquet.com>
2023-04-22 14:27:24 -0700

    Implemented Home Page

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -21,7 +21,13 @@ import {
     SliderMark,
     Divider,
     Spinner,
+    Card,
+    CardBody,
+    Image
  } from '@chakra-ui/react'
+
+import { Icon, StarIcon } from '@chakra-ui/icons'
+import { MdOutlineFastfood, MdAttractions } from 'react-icons/md'
 import "./HomePage.css";
 
 function HomePage() {
@@ -38,6 +44,8 @@ function HomePage() {
 
     const [trip, setTrip] = useState(null)
 
+    const [tripAdded, setTripAdded] = useState(false)
+
 
     function timeout(delay) {
         return new Promise( res => setTimeout(res, delay) );
@@ -49,7 +57,32 @@ function HomePage() {
         // CALL API TO GET TRIP
         await timeout(2000); //for 1 sec delay
 
-        
+        setTrip({
+            userID: "123",
+            location: location,
+            hotel: "Cardboard Box",
+            total_price: 999.98,
+            budget: budget,
+            num_likes: 0,
+
+            // list of days in the trip
+            days: [
+                {
+                    attraction: "Eiffel Tower",
+                    food1: "La brasserie du 7eme",
+                    food2: "McDonalds",
+                    daily_budget: "40",
+                    miles_traveled: "10"
+                },
+                {
+                    attraction: "Notre Dame",
+                    food1: "Chicken Kebab",
+                    food2: "Burger King",
+                    daily_budget: "100",
+                    miles_traveled: "25"
+                },
+            ]
+        })
         setIsGenerating(false)
         
     }
@@ -62,14 +95,18 @@ function HomePage() {
                 value={location}
                 onChange={e => setLocation(e.target.value)}
                 placeholder='Enter your dream trip location'
-                w={400}
+                backgroundColor="white"
+                color="black"
+                w={300}
             />
             <NumberInput>
                 <NumberInputField
                     value={budget}
                     onChange={e => setBudget(e.target.value)}
                     placeholder='Enter your buget ($)'
-                    w={400}
+                    backgroundColor="white"
+                    color="black"
+                    w={300}
                 />
             </NumberInput>
             <NumberInput>
@@ -77,14 +114,16 @@ function HomePage() {
                     value={numberOfDays}
                     onChange={e => setNumberOfDays(e.target.value)}
                     placeholder='Enter your trip length (days)'
-                    w={400}
+                    backgroundColor="white"
+                    color="black"
+                    w={300}
                 />
             </NumberInput>
         </HStack>
         <Accordion allowMultiple w={500}>
             <AccordionItem>
                 <AccordionButton>
-                    <Text>Advanced Options</Text>
+                    <Text fontWeight='bold'>Advanced Options</Text>
                     <AccordionIcon />
                 </AccordionButton>
                 <AccordionPanel pb={4}>
@@ -129,13 +168,77 @@ function HomePage() {
             variant='solid'
             w={300}
             onClick={generateTrip}
-            isDisabled={location==="" | budget === null | numberOfDays === null | isGenerating}
+            // isDisabled={location==="" | budget === null | numberOfDays === null}
+            isLoading={isGenerating}
+            spinner={<Spinner size="md"/>}
         >
             Generate my trip!
         </Button>
-        {isGenerating && (<Spinner size='xl'/>)}
         {trip != null && !isGenerating && (
-            <Heading>Trip!</Heading>
+            <VStack border='1px' borderColor='white' borderRadius={10} align='strech' w={900}  padding="20px" backgroundColor="gray.200">
+                <HStack spacing='auto'>
+                    <Heading fontSize='3xl' color="black">NEW budgeTRIP!</Heading>
+                    <Button
+                        leftIcon={<StarIcon />}
+                        color='white'
+                        backgroundColor={tripAdded ? 'red.500' : 'brand.200'}
+                        _hover={{ bg: "gray.400", color: "white" }}
+                        _active={{ bg: "gray.400"}}
+                        // borderColor='brand.400'
+                        // border={tripAdded ? "0px" : "1px"}
+                        variant='solid'
+                        fontSize='xl'
+                        onClick={() => setTripAdded(!tripAdded)}
+                        width={250}
+                    >
+                        {tripAdded ? "Remove from profile" : "Add to profile!"}
+                        </Button>
+                </HStack>
+                <Card key={-1} border='1px' borderColor='white' borderRadius={20} backgroundColor='white'>
+                    <CardBody>
+                        <HStack spacing='auto'>
+                            <Text fontSize='xl' color="black" fontWeight="bold">Total Price: ${trip.total_price}</Text>
+                            <Text fontSize='xl' color="black" fontWeight="bold">Hotel: {trip.hotel}</Text>
+                        </HStack>
+                    </CardBody>
+                </Card>
+                {trip.days.map((element, index) => {
+                    return (
+                        <Card key={index} border='1px' borderColor='white' borderRadius={20} backgroundColor='white'>
+                            <CardBody>
+                                <HStack spacing='auto'>
+                                    <VStack align='stretch'>
+                                        <Text fontSize='xl' fontWeight='bold' color='black'>Day {index}</Text>
+                                        <HStack>
+                                            <Icon as={MdAttractions} color="black"/>MdAttractions
+                                            <Text color="black">Attraction: {element.attraction}</Text>
+                                        </HStack>
+                                        <HStack>
+                                            <Icon as={MdOutlineFastfood} color="black"/>
+                                            <Text color="black">Lunch: {element.food1}</Text>
+                                        </HStack>
+                                        <HStack>
+                                            <Icon as={MdOutlineFastfood} color="black"/>
+                                            <Text color="black">Dinner: {element.food2}</Text>
+                                        </HStack>
+                                        <HStack spacing='30px'>
+                                            <Text color='black' fontWeight='bold'>Price: ${element.daily_budget}</Text>
+                                            <Text color='black' fontWeight='bold'>Distance: {element.miles_traveled} miles</Text>
+                                        </HStack>
+                                    </VStack>
+                                    <Image
+                                        objectFit='cover'
+                                        src='https://images.unsplash.com/photo-1531403009284-440f080d1e12?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
+                                        alt='Chakra UI'
+                                        maxH={150}
+                                        maxW={225}
+                                    />
+                                </HStack>
+                            </CardBody>
+                        </Card>
+                    )
+                })}
+            </VStack>
         )}
     </VStack>
     );
