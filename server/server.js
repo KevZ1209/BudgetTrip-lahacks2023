6caa39e418ef6e87f8fc5c3cee25e813129ca2a0
KevZ1209 <kevinbowditch11@gmail.com>
2023-04-21 23:38:54 -0700

    add schemas & models

--- server/server.js
+++ server/server.js
@@ -21,6 +21,46 @@ async function main() {
     mongoose.connect("mongodb://127.0.0.1:27017/trip_db")
 }
 
+/* ***************** SCHEMAS AND MODELS ***************** */
+
+/* USER SCHEMA AND MODEL */
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String,
+    name: String,
+    num_likes: Number
+})
+
+// create user model
+const User = mongoose.model("User", userSchema)
+
+/* TRIP SCHEMA AND MODEL */
+const tripSchema = new mongoose.Schema({
+    location: String,
+    hotel: String,
+    total_price: Number,
+    budget: Number,
+    num_likes: Number,
+
+    // list of days in the trip
+    days: [{
+        attraction: String,
+        food1: String,
+        food2: String,
+        daily_budget: Number,
+        miles_traveled: Number
+    }]
+})    
+
+// create trip model
+const Trip = mongoose.model("Trip", tripSchema)
+
+/* ***************** API ENDPOINTS ***************** */
+
+// test home route
+app.get("/", function(req, res) {
+    res.send("Hello World!")
+})
 
 app.listen(8000, function(req, res) {
     console.log("Listening on port 8000")
